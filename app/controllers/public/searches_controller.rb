0c0697b7e346c1410ef068f12523b883d4ae5427
EC2 Default User <108472584+seiya0507@users.noreply.github.com>
2022-09-23 10:32:55 +0000

    [Update] public レイアウト修正

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -8,7 +8,7 @@ class Public::SearchesController < ApplicationController
     if @model == "user"
       @records = User.search_for(@content, @method)
     else
-      @records = Car.search_for(@content, @method)
+      @records = Car.search_for(@content, @method).page(params[:page]).order(created_at: :desc)
     end
     render "search"
   end
