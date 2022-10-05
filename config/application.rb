f4d121831808571bb3dc049049c7e17b71bb8ae0
EC2 Default User <108472584+seiya0507@users.noreply.github.com>
2022-10-05 16:14:53 +0000

    [Update] application.rb 日本時間対応, レイアウト修正

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,9 @@ module Cars
     #日本語設定
     config.i18n.default_locale = :ja
 
+    config.time_zone = 'Tokyo'
+
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
